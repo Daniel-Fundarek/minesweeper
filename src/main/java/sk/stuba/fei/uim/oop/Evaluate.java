3d9bbb68bc0bba3df80fb433ba59fa3ed08cdf36
Daniel-Fundarek <xfundarekd@stuba.sk>
2021-03-02 18:46:30 +0100

    save pred zmenenim pola

--- src/main/java/sk/stuba/fei/uim/oop/Evaluate.java
+++ src/main/java/sk/stuba/fei/uim/oop/Evaluate.java
@@ -1,5 +1,48 @@
 package sk.stuba.fei.uim.oop;
 
 public class Evaluate {
+    char[][] board;
+
+
+    public Evaluate(char[][] board,int x, int y){
+        this.board=board;
+        this.x = x;
+        this.x = x;
+    }
+
+    private void evalSquaresAround(int x, int y){
+
+        if (board[x][y]=='0') {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+
+                    if(board[x-1][y-1]=='0')
+
+                }
+            }
+        }
+        else
+            ;
+    }
+    private void checkBoarder(int x, int y){
+        int hore = 0;
+        int dole = 0;
+        int doprava = 0;
+        int dolava = 0;
+        if(x==0){
+             hore=1;   //kraj
+        }
+        if(y==0){
+             dolava=1;   //kraj
+        }
+        if(x== board.length){
+             dole=1;   //kraj
+        }
+        if(y== board[1].length){
+             doprava=1;   //kraj
+        }
+
+
+    }
 
 }
--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -3,7 +3,8 @@ package sk.stuba.fei.uim.oop;
 public class Main {
     public static void main(String[] args) {
 
-        Table table = new Table(20,20,100);
-        table.createBoard();
+        Table x = new Table();
+
+        x.createBoard();
     }
 }
